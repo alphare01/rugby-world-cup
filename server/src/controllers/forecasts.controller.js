0c52633cba6b73746f31655204e679fd7b7229ae
raphael innocenti <raphael.innocenti@gmail.com>
2023-01-11 13:13:20 +0100

    delete improvements.js and reset pools when refresh

--- server/src/controllers/forecasts.controller.js
+++ server/src/controllers/forecasts.controller.js
@@ -1,12 +1,15 @@
 //-----------REQUIRE-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+const poolTablesDataBase = require('../schemata/pool.tables.mongo');
 const {postForecasts, getAllForecasts} = require('../models/forecasts.model');
-const {countWDL, countBP, countPF, countPA, countDIFF, countPOINTS, countTF, countPL, getAllPoolTables} = require('../models/pool.tables.model');
+const {loadPoolTablesData, countWDL, countBP, countPF, countPA, countDIFF, countPOINTS, countTF, countPL, getAllPoolTables} = require('../models/pool.tables.model');
 
 //-----------FUNCTION----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 async function httpPostAllForecasts(req, res) {
     const forecasts = req.body;
+    await poolTablesDataBase.deleteMany({});
+    await loadPoolTablesData();
     await postForecasts(forecasts);
     const forecastsData = await getAllForecasts();
     await countWDL(forecastsData);
